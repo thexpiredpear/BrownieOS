847e5e6f12940db268a3bea3a7659716a741a7e0
Rishi <rpudipeddi@eastsideprep.org>
2023-12-22 22:47:18 -0400

    hpet counts up @ freq!

--- kernel/kernel/drivers/hpet.c
+++ kernel/kernel/drivers/hpet.c
@@ -11,6 +11,9 @@
 #include <mm/vmm.h>
 #include <mm/kmm.h>
 
+uint32_t clk_per = 0;
+uint32_t freq = 0;
+
 void init_hpet() {
     hpet_t* hpet = get_hpet();
     if(hpet == NULL) {
@@ -24,7 +27,7 @@ void init_hpet() {
     volatile uint64_t* gen_conf = (volatile uint64_t*)(hpet_addr + 0x10);
     volatile uint64_t* main_cnt = (volatile uint64_t*)(hpet_addr + 0xF0);
     volatile uint64_t* tim_n_conf_base = (volatile uint64_t*)(hpet_addr + 0x100);
-    uint32_t clk_per = (uint32_t)((*gen_cap >> 32) & 0xFFFFFFFF);
+    clk_per = (uint32_t)((*gen_cap >> 32) & 0xFFFFFFFF);
     uint16_t ven_id = (uint16_t)((*gen_cap >> 16) & 0xFFFF);
     uint8_t leg_cap = (uint8_t)((*gen_cap >> 15) & 0x1);
     uint8_t cnt_cap = (uint8_t)((*gen_cap >> 13) & 0x1);
@@ -33,29 +36,35 @@ void init_hpet() {
     printf("CLK_PER: 0x%x | VEN_ID: 0x%x | LEG_CAP: %i | CNT_CAP: %i | NUM_TIM: %i | REV_ID: %i\n\n", clk_per, ven_id, leg_cap, cnt_cap, num_tim, rev_id);
     *gen_conf &= ~((uint64_t)0b11);
     *main_cnt = 0;
+    uint64_t tim_n_disable_mask = ~((0b11111 << 9) | (1 << 2) | (1 << 3) | (1 << 6) | (1 << 6));
+    uint64_t tim_n_enable_mask = (0b10 << 9) | (1 << 2) | (1 << 3) | (1 << 6);
     for(uint8_t i = 0; i < num_tim; i++) {
+        bool flag = false;
         printf("---- TIMER %i ----\n", i);
         volatile uint64_t* tim_n_conf = (volatile uint64_t*)((uint32_t)tim_n_conf_base + (0x20 * i));
         uint32_t cap_apic = (uint32_t)((*tim_n_conf >> 32) & 0xFFFFFFFF);
-        uint8_t cur_apic = (uint8_t)((*tim_n_conf >> 9) & 0x1F);
-        uint8_t cur_size = (uint8_t)((*tim_n_conf >> 8) & 0x1);
         uint8_t cap_size = (uint8_t)((*tim_n_conf >> 5) & 0x1);
         uint8_t cap_per = (uint8_t)((*tim_n_conf >> 4) & 0x1);
-        for(int j = 0; j < 32; j++) {
-            if((cap_apic >> j) & 0x1 == 1) {
-                printf("IRQ%i CAPABLE\n", j);
-            }
-        }
         if(cap_size != 1) {
-            panic("TIMER NOT 64 BIT CAPABLE");
-        } else {
-            printf("TIMER 64 BIT CAPABLE\n");
+            printf("TIMER NOT 64 BIT CAPABLE\n");
+            flag = true;
         }
         if(cap_per != 1) {
-            panic("TIMER NOT PERIODIC CAPABLE");
-        } else {
-            printf("TIMER PERIODIC CAPABLE\n");
+            printf("TIMER NOT PERIODIC CAPABLE\n");
+            flag = true;
         }
-        printf("CUR_APIC: IRQ%i | CUR_SIZE: %i\n\n", cur_apic, (cur_size == 1) ? 64 : 32);
+        if((cap_apic >> 2) & 0x1 != 1) {
+            printf("TIMER NOT IRQ2 CAPABLE\n");
+        }
+        if(flag) {
+            panic("TIMER SETUP FAILED");
+        }
+        // clear bits 2 3 8 9-13
+        *tim_n_conf &= tim_n_disable_mask;
+        printf("%x\n", tim_n_disable_mask);    
     }
+    freq = (uint32_t)(1000000000000000 / clk_per);
+    printf("HPET FREQUENCY: %i\n", freq);
+    *gen_conf |= 0x1;
+    *tim_n_conf_base |= tim_n_enable_mask;
 }
