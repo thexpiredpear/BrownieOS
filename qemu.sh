a0c016b3716db0c0bf0f4aa0caae140c1f0938f4
thexpiredpear <thexpiredpear@gmail.com>
2025-10-11 23:50:02 -0700

    wow the issue was that qemu didn't provide enough memory jeez

--- qemu.sh
+++ qemu.sh
@@ -4,11 +4,11 @@ set -e
 . ./iso.sh
 if [ $1 -eq 1 ]
 then
- qemu-system-$(./target-triplet-to-arch.sh $HOST) -smp 2 -s -S -cdrom brownieos.iso &
+ qemu-system-$(./target-triplet-to-arch.sh $HOST) -m 3G -smp 2 -s -S -cdrom brownieos.iso &
  gdb -ex "target remote localhost:1234" -ex "symbol-file sysroot/boot/brownieos.kernel" -ex "layout src"
 elif [ $1 -eq 2 ]
 then
- qemu-system-$(./target-triplet-to-arch.sh $HOST) -smp 2 -cdrom brownieos.iso -monitor stdio
+ qemu-system-$(./target-triplet-to-arch.sh $HOST) -m 3G -smp 2 -cdrom brownieos.iso -monitor stdio
 else
- qemu-system-$(./target-triplet-to-arch.sh $HOST) -smp 2 -cdrom brownieos.iso
+ qemu-system-$(./target-triplet-to-arch.sh $HOST) -m 3G -smp 2 -cdrom brownieos.iso
 fi
